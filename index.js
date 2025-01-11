0ef658e5c4c3fc87896098cfb06ca9e823722f0b
abunaserkayes0 <20234203080@cse.bubt.edu.bd>
2025-01-11 15:57:49 +0600

    feat:implement sorting touristspot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const client = require("./mongodb/mongoClient");
+const { ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,8 +31,40 @@ app.get("/", (req, res) => {
 });
 
 app.get("/tourist", async (req, res) => {
+  const { sortValue } = req.query;
   try {
-    const result = await req.db.collection("tourist").find({}).toArray();
+    let sortOrder = 1;
+
+    if (sortValue === "asc") {
+      sortOrder = 1;
+    } else if (sortValue === "dsc") {
+      sortOrder = -1;
+    }
+    const result = await req.db
+      .collection("tourist")
+      .find()
+      .sort({ averageCost: sortOrder })
+      .toArray();
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// app.get("/tourist", async (req, res) => {
+//   try {
+//     const result = await req.db.collection("tourist").find({}).toArray();
+//     res.send(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+app.get("/tourist/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = { _id: new ObjectId(id) };
+    const result = await req.db.collection("tourist").findOne(query);
     res.send(result);
   } catch (error) {
     console.log(error);
@@ -48,7 +81,7 @@ app.post("/tourist", async (req, res) => {
     shortDescription,
     spotName,
     totalVisitorsPerYear,
-    travelCost,
+    travelTime,
     userEmail,
     userName,
   } = req.body;
@@ -62,7 +95,7 @@ app.post("/tourist", async (req, res) => {
     shortDescription,
     spotName,
     totalVisitorsPerYear,
-    travelCost,
+    travelTime,
     userEmail,
     userName,
   };
